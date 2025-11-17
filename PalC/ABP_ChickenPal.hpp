0e00d97732e4e3203cb7d940047367bcea7f5dd3
namelessphoto <3806969388@qq.com>
2025-11-17 21:03:37 +0800

    Create ABP_ChickenPal.hpp

---
+++ PalC/ABP_ChickenPal.hpp
@@ -0,0 +1,12 @@
+#ifndef UE4SS_SDK_ABP_ChickenPal_HPP
+#define UE4SS_SDK_ABP_ChickenPal_HPP
+
+struct FAnimBlueprintGeneratedConstantData : public FAnimBlueprintGeneratedConstantData
+{
+};
+
+class UABP_ChickenPal_C : public UABP_MonsterBase_C
+{
+};
+
+#endif
